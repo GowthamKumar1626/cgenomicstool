fb4868f29cf4650125954f063142fcd240c84299
Gowtham Kumar <gowthamkumar20200@gmail.com>
2021-07-21 22:53:15 +0530

    Update V2

--- crosstab/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CrosstabConfig(AppConfig):
-    name = 'crosstab'
--- crosstab/form.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-
-class UploadFileForm(forms.Form):
-    file = forms.FileField(widget=forms.FileInput(attrs={'class': 'form-control', 'id': 'inputGroupFile02'}))
