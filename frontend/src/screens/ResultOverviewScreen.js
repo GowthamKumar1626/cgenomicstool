611dce7d303f0e28dd08ce78807e8c727aa39fd9
Gowtham Kumar <gowthamkumar20200@gmail.com>
2021-08-11 16:01:41 +0530

    Added Color code card in results overview

--- frontend/src/screens/ResultOverviewScreen.js
+++ frontend/src/screens/ResultOverviewScreen.js
@@ -2,11 +2,13 @@ import React, { useEffect } from "react";
 import { Card, Row, Col } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { listResultDetails } from "../actions/resultsListActions";
-import { Markup } from "interweave";
+import { codes } from "../constants/GeneClasses";
+// import { Markup } from "interweave";
+import Loader from "../components/Loader";
 
 function ResultOverviewScreen({ match, history, location }) {
   const resultDetails = useSelector((state) => state.resultDetails);
-  const { result } = resultDetails;
+  const { loading, result } = resultDetails;
 
   const userLogin = useSelector((state) => state.userLogin);
   const { userInfo } = userLogin;
@@ -17,12 +19,16 @@ function ResultOverviewScreen({ match, history, location }) {
       history.push("/login");
     }
     dispatch(listResultDetails(match.params.id));
+    // const dataframeElement = (document.getElementsByClassName(
+    //   "dataframe"
+    // )[0].innerHTML = result.upload_results);
   }, [dispatch, match, history, userInfo]);
 
   return (
     <div>
       <h2>Result Overview</h2>
       <Card className="my-3">
+        {loading && <Loader />}
         <Card.Header>
           <h2>Result ID</h2>
           {result.result_id}
@@ -36,14 +42,50 @@ function ResultOverviewScreen({ match, history, location }) {
           </Row>
         </Card.Body>
       </Card>
-      <Card className="my-3">
+      <Row>
+        <Col md={3}>
+          <Card>
+            <Card.Header>Color Mappings</Card.Header>
+            <Card.Body className="overflow-auto">
+              <div>
+                {codes.map((code) => (
+                  <Row key={code.name}>
+                    <Col md={9}>
+                      <p>{code["geneName"]}</p>
+                    </Col>
+                    <Col md={3}>
+                      <div
+                        style={{
+                          backgroundColor: code["colorCode"],
+                          height: 20,
+                          width: 40,
+                        }}
+                      ></div>
+                    </Col>
+                  </Row>
+                ))}
+              </div>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col md={9}>
+          <Card>
+            <Card.Header>HeatMap</Card.Header>
+            <Card.Body></Card.Body>
+          </Card>
+        </Col>
+      </Row>
+
+      {/* <Card>
         <Card.Header>
-          <h2>Crosstab Table</h2>
+          <h2>Details</h2>
         </Card.Header>
         <Card.Body className="overflow-auto">
+        <Card.Body>
+          <div className="dataframe"></div>
           <Markup content={result.upload_results} />
         </Card.Body>
-      </Card>
+      </Card> */}
     </div>
   );
 }
